a757a5937a3194c32c93981a551fb762d44e92d3
Renato-sonic-e-games <rennerutame009@gmail.com>
2026-02-14 16:35:15 -0300

    Add files via upload

---
+++ exercicio4/exercicio4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double total = 0;
+            int[] notas = new int[10];
+            Console.WriteLine("Insira a nota as 10 notas do aluno para calcular sua média");
+            for (int i = 0; i < 10; i++)
+            {
+                notas[i] = int.Parse(Console.ReadLine());
+                if ((notas[i] > 10) || (notas[i] < 0))
+                {
+                    i--;
+                    Console.WriteLine("somente insira valores entre 0 a 10");
+                }
+                else total = total + notas[i];
+
+            }
+            double media;
+            media = total/10 ;
+            Console.WriteLine("a média desse aluno é:" + media);
+        }
+    }
+}
---
+++ exercicio8/exercicio8/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double salario;
+            string cargo = "bla";
+            double percentual = 0;
+            Console.WriteLine("Digite o salário do funcionário");
+            salario = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o código do cargo do funcionário (101-104)");
+            int code = int.Parse(Console.ReadLine());
+            switch (code)
+            {
+                case 101:
+                    cargo = "admnistrador";
+                    percentual = 0.008;
+                    break;
+                case 102:
+                    cargo = "engenheiro";
+                    percentual = 0.0065; 
+                    break;
+
+                case 103:
+                    cargo = "médico";
+                    percentual = 0.006; 
+                    break;
+
+                case 104:
+                    cargo = "outro cargo";
+                    percentual = 0.0055;
+                    break;
+
+                default:
+                    Console.WriteLine("código inexistente");
+                    return;
+            }
+            double aument = salario * percentual;
+            double novsalario = salario + aument;
+            Console.WriteLine("Cargo: " + cargo);
+            Console.WriteLine("Salário antigo: " + salario);
+            Console.WriteLine("novo salário: " + novsalario);
+            Console.WriteLine("diferença: "+aument);
+        }
+     }
+   }
